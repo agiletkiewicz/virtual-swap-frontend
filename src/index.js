484b7b903f21afc0040c472fa5829b55a81b0b5f
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-13 14:49:09 -0400

    DOM content loaded listener

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
-const BACKEND_URL = 'http://localhost:3000';
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("loaded")
+})
+
+
+const BACKEND_URL = 'http://localhost:3000/api/v1';
 fetch(`${BACKEND_URL}/events`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
