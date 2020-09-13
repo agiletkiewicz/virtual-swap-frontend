1b9c6b08b359d1e1c5c864518598ea04b3bcd42e
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-13 15:09:27 -0400

    add create event form

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log("loaded")
+  test ();
 })
 
 
 const BACKEND_URL = 'http://localhost:3000/api/v1';
-fetch(`${BACKEND_URL}/events`)
+
+function test() {
+  fetch(`${BACKEND_URL}/events`)
   .then(response => response.json())
   .then(parsedResponse => console.log(parsedResponse));
+}
+
+
