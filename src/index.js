cd74530acfe50013f652d844585cb9439e17befd
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-14 17:40:06 -0400

    add events to join event form

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   test ();
 
-  const createEventForm = document.getElementById('create-event-form');
-
   createEventForm.addEventListener("submit", (event) => createFormHandler(event));
+
 })
 
 
 const BACKEND_URL = 'http://localhost:3000/api/v1';
+const createEventForm = document.getElementById('create-event-form');
 
 function test() {
   fetch(`${BACKEND_URL}/events`)
   .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+  .then(parsedResponse => addEventsToForm(parsedResponse));
+}
+
+function addEventsToForm(allEvents) {
+  for (const event of allEvents.data) {
+    const eventOptions = document.getElementById("events")
+    let newOption = new Option(event.attributes.name, event.id);
+    eventOptions.add(newOption,undefined);
+  } 
 }
 
 function createFormHandler(event) {
@@ -23,7 +31,32 @@ function createFormHandler(event) {
 }
 
 function postFetch(name, rules) {
-  console.log(name, rules);
+  fetch(`${BACKEND_URL}/events`, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      name: name, 
+      rules: rules,
+    })
+  })
+  .then(response => response.json())
+  .then(event => {
+    renderEvent(event);
+  })
+}
+
+function renderEvent(event) { 
+  const eventData = event.data.attributes;
+
+  const header = document.createElement("h1");
+  header.innerText = eventData.name;
+  const subheader = document.createElement("h2");
+  subheader.innerText = eventData.rules;
+
+  createEventForm.style.display = 'none';
+  const eventContainer = document.querySelector("#event-container");
+  eventContainer.appendChild(header);
+  eventContainer.appendChild(subheader);
 }
 
 
