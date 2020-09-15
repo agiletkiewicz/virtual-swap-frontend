18b8db735de371dee42c672ab7cacc60539b6e2a
agiletkiewicz <a.letkiewicz@gmail.com>
2020-09-15 16:30:22 -0400

    add users to select user form

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const createEventForm = document.getElementById('create-event-form');
 const viewEventForm = document.getElementById('view-event-form');
+const eventContainer = document.querySelector("#event-container");
+const userForm = document.querySelector("#select-user-form");
 
 function fetchEvents() {
   new Adapter('/events').getRequest()
@@ -69,19 +71,41 @@ function renderEvent(event) {
 
   createEventForm.style.display = 'none';
   viewEventForm.style.display = 'none';
-  const eventContainer = document.querySelector("#event-container");
   eventContainer.appendChild(header);
   eventContainer.appendChild(subheader);
 
-  renderUsers(eventId);
+  getUsers(eventId);
 }
 
-function renderUsers(eventId) {
+function getUsers(eventId) {
   new Adapter(`/events/${eventId}/users`).getRequest()
   .then(users => {
     for (const element of users.data) {
       new User(element)
     }
+    renderUserSelectForm()
   })
-  console.log(User.all)
+  // console.log(User.all)
+  // renderUserSelectForm();
+}
+
+function renderUserSelectForm() {
+  const select = document.createElement("select");
+  select.name = "users";
+  select.id = "users"
+  User.all.forEach( (user) => {
+    let newOption = new Option(user.name, user.id);
+    select.appendChild(newOption,undefined);
+  }) 
+
+  const label = document.createElement("label");
+  label.innerHTML = "Choose an existing user: "
+  label.htmlFor = "users";
+
+  const submit = document.createElement("input");
+  submit.setAttribute('type',"submit");
+  submit.setAttribute('value',"Submit");
+
+  userForm.appendChild(label).appendChild(select);
+  userForm.appendChild(submit);
 }
